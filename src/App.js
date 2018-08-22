bc5672ddef6596f32f25cb6717afafb70e17bf5a
Rupesh Jaiswal <RupeshJ@citiustech.com>
2018-08-22 18:06:41 +0530

    basic layout of app

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Tabs, Tab, Badge} from 'react-bootstrap';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: 1
+    }
+  }
+  handleSelect = (key) => {
+    this.setState({key});
+  }
+  render() {
+    const teams=(
+      <div ><h4 className="teams-tab">Teams</h4><Badge>10</Badge></div>
+    )
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Cluster League</h1>
+        </header>
+        <div className="container">
+          <Tabs
+            activeKey={this.state.key}
+            onSelect={this.handleSelect}
+            id="controlled-tab-example"
+          >
+            <Tab eventKey={1} title={<h4>Matches</h4>}>
+              Under construction
+            </Tab>
+            <Tab eventKey={2} title={<h4>Stats</h4>}>
+              Tab 2 content
+            </Tab>
+            <Tab eventKey={3} title={teams} >
+              Tab 3 content
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
